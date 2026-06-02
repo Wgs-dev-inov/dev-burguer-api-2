0e42fd99dd7a242b3cf862ab749b2c7a50bdefe0
dev-Wwill <wgstpl@gmail.com>
2026-06-02 18:09:05 -0300

    concluindo a config do banco de dados/sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'admin',
+  password: '123456',
+  database: 'dev-burguer-db2',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
