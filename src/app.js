db9757b671f1d3b420ed257fe28fd72505dcc747
dev-Wwill <wgstpl@gmail.com>
2026-06-05 18:22:58 -0300

    constroller de usuario criado

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import routes from './routes.js';
 
 const app = express();
-app.use(routes);
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 export default app;
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,9 @@
 import { Router } from 'express';
-import { v4 } from 'uuid';
-import User from './app/models/User.js';
+import UserController from './app/controllers/UserController.js';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = {
-    id: v4(),
-    name: 'willian',
-    email: 'willian@willian',
-    password_hash: '123456',
-    admin: false,
-  };
-  await User.create(user);
+routes.post('/users', UserController.store);
 
-  res.status(201).json(user);
-});
-export default routes;
+export default routes
+
