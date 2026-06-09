f0e1a82eaf9a0381129e84df4bf5050423aad356
dev-Wwill <wgstpl@gmail.com>
2026-06-09 18:48:59 -0300

    bcript instalado e configurado

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,9 +1,33 @@
 import { v4 } from 'uuid';
+import * as Yup from 'yup';
 import User from '../models/User.js';
+import bcrypt from 'bcrypt';
 
 class UserController {
   async store(request, response) {
-    const { name, email, password_hash, admin } = request.body;
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().min(6).required(),
+      admin: Yup.boolean(),
+    });
+    try {
+      schema.validateSync(request.body, { abortEarly: false, strict: true });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+
+    const { name, email, password, admin } = request.body;
+
+    const userExists = await User.findOne({
+      where: { email },
+    });
+
+    if (userExists) {
+      return response.status(400).json({ error: 'User already exists' });
+    }
+
+    const password_hash = await bcrypt.hash(password, 10);
 
     const user = await User.create({
       id: v4(),
